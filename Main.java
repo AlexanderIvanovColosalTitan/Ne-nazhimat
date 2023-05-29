5911a11e7b4b76082fb6fde3cbade7efa6e4adc9
AlexanderIvanovColosalTitan <112861088+AlexanderIvanovColosalTitan@users.noreply.github.com>
2023-05-29 21:20:23 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Hash table = new Hash();
-        table.put(1, 10);
-        table.put(2, 20);
-        table.put(3, 30);
-        table.put(4, 40);
-        // выводит 20
-        System.out.println(table.get(1));
-        table.remove(2);
-        System.out.println(table.get(2));
-        System.out.println(table.get(4));
+        HashTable hashtable = new HashTable();
+        hashtable.put(1, 10);
+        hashtable.put(2, 20);
+        hashtable.put(3, 30);
+        System.out.println("Size: " + hashtable.size());
+        System.out.println(hashtable.get(3));
+        hashtable.remove(2);
+        System.out.println("Size: " + hashtable.size());
     }
-
 }
+
